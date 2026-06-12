e6de66f0b3df7e77e8963cc02fbc4f11fbda6511
manuele <manuele.pedicillo@gmail.com>
2026-06-12 22:19:41 +0200

    Updating the frontend and creating some new functionalities

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 from app.database import engine, Base
 from app.routers import (auth, products, favorites, supermarkets, recipes, recipe_items,
-                         cart, users, shopping_history, flyer_catalog, profile, recipe_smart)
+                         cart, users, shopping_history, flyer_catalog, profile, recipe_smart, admin)
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -68,3 +68,4 @@ app.include_router(shopping_history.router)
 app.include_router(flyer_catalog.router)
 app.include_router(profile.router)
 app.include_router(recipe_smart.router)
+app.include_router(admin.router)
